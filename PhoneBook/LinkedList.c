65014f3d3d1b5084aa6c0c68c01832dafc3f5220
samyoahri <samyoahri@gmail.com>
2023-10-27 20:15:20 +0900

    I created simple single linked list

---
+++ PhoneBook/LinkedList.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_LENGTH 100
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <limits.h>
+
+#include"LinkedList.h"
+
+void printList(ListNode* head) {
+	for (ListNode* p = head; p != NULL; p = p->pNext) {
+		printf("|| %s | %s || -> ", p->columinfo.name, p->columinfo.phone);
+	}
+	printf("NULL\n");
+}
+ListNode* insertFirst(ListNode* head, ColumInfo value) {
+	ListNode* added = (ListNode*)malloc(sizeof(ListNode));
+
+	strcpy(added->columinfo.name, value.name);
+	strcpy(added->columinfo.phone, value.phone);
+	added->pNext = head;
+
+	head = added;
+
+	return added;
+}
+ListNode* deleteFirst(ListNode* head) {
+	ListNode* removed = (ListNode*)malloc(sizeof(ListNode));
+
+	if (head == NULL)	return NULL;
+
+	removed = head;
+	head = head->pNext;
+	free(removed);
+
+	return head;
+}
---
+++ PhoneBook/LinkedList.h
@@ -0,0 +1,26 @@
+#ifndef __LINKEDLIST_H__
+#define __LINKEDLIST_H__
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_LENGTH 100
+
+#pragma pack(push, 1)
+typedef struct ColumInfo {
+	char name[MAX_LENGTH];
+	char phone[MAX_LENGTH];
+}ColumInfo;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct ListNode {
+	struct ColumInfo columinfo;
+	struct ListNode* pNext;
+}ListNode;
+#pragma pack(pop)
+
+void printList(ListNode* head);
+ListNode* insertFirst(ListNode* head, ColumInfo value);
+ListNode* deleteFirst(ListNode* head);
+
+
+
+#endif
