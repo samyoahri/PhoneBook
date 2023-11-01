ba200e70434a61245a680e1f3f11cf157f843a78
samyoahri <samyoahri@gmail.com>
2023-11-02 01:50:26 +0900

    Bug fix BinarySearchTree_Test code

--- PhoneBook/BinarySearchTree_Test.c
+++ PhoneBook/BinarySearchTree_Test.c
@@ -75,19 +75,37 @@ double testBinarySearchTree_delete(TreeNode** root, int nNum) {
 		strcpy(data.name, sName);
 		strcpy(data.phone, sPhone);
 
-		*root = insertBinarySearchTreeNode(*root, data);
+		*root = deleteBinarySearchTreeNode(*root, data);
 	}
 	clock_t end = clock();
 	return (double)(end - start) / CLOCKS_PER_SEC;
 }
+
 double testBinarySearchTree_search(TreeNode** root, int nNum) {
+	int cnt = 0;
 	clock_t start = clock();
 	for (int i = 0; i < nNum; ++i) {
-		
+		//search();
+		//printf("search();\n");
+		char szName[MAX_LENGTH];
+		char szPhone[MAX_LENGTH];
+		ColumInfo data;
 
-		//inorder(root);
-		//printf("--------------------------------------------\n");
+		sprintf(szName, "samyoahri: %d", i);
+		sprintf(szPhone, "010-xxxx-xxxx: %d", i);
+
+		strcpy(data.name, szName);
+		strcpy(data.phone, szPhone);
+
+		//*root = searchBinarySearchTreeNode(*root, data);
+
+		//TreeNode* tmp = *root;
+		TreeNode* tmp = searchBinarySearchTreeNode(*root, data);
+		if (strcmp(tmp->columinfo.name, szName) == 0) {
+			++cnt;
+		}
 	}
 	clock_t end = clock();
+	printf("testBST_search Ã£Àº°¹¼ö: %d\n", cnt);
 	return (double)(end - start) / CLOCKS_PER_SEC;
 }
